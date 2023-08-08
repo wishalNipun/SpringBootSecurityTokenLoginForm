a5cd1fec78b55b5cab0229a14a3a80c633074b2d
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-08 15:41:50 +0530

    updated jwtfilter and created jwtService

---
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.wishal.SpringSecurityTokenLoginForm.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return  null;
+    }
+}
