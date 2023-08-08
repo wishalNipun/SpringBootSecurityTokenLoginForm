520d85cb4171d413e7f4f0813dd6dd367b025c9a
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-08 16:26:29 +0530

    jwtservice refactored

--- src/main/java/com/wishal/SpringSecurityTokenLoginForm/config/JwtService.java
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/config/JwtService.java
@@ -2,14 +2,19 @@ package com.wishal.SpringSecurityTokenLoginForm.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private  static final String SECRET_KEY="";
+    private  static final String SECRET_KEY="5e3105ab73d8c46906a079e52f98449f32283e7b0cf4d6f9746f71dd53713b49";
     public String extractUsername(String token) {
-        return  null;
+        return  extractClaim(token,Claims::getSubject);
     }
     private Claims extractAllClaims (String token){
         return Jwts
@@ -19,4 +24,14 @@ public class JwtService {
                 .parseClaimsJws(token)
                 .getBody();
     }
+
+    public <T> T extractClaim (String token, Function<Claims,T>claimsResolver ){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
