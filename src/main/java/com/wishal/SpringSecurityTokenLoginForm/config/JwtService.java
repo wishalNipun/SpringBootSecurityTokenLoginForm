ff6abafadc9e4e9400b14ede8b862d23d86d63d3
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-08 16:07:04 +0530

    added token dependacy and updated jwtservice

--- src/main/java/com/wishal/SpringSecurityTokenLoginForm/config/JwtService.java
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/config/JwtService.java
@@ -1,11 +1,22 @@
 package com.wishal.SpringSecurityTokenLoginForm.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    private  static final String SECRET_KEY="";
     public String extractUsername(String token) {
         return  null;
+    }
+    private Claims extractAllClaims (String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
