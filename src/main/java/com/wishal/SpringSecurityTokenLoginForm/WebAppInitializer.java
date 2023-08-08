8b64552e70e50f47d81fa6449d3f75a1c07111ef
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-08 15:10:50 +0530

    first commit and started project also creating enity

---
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/WebAppInitializer.java
@@ -0,0 +1,13 @@
+package com.wishal.SpringSecurityTokenLoginForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppInitializer.class, args);
+	}
+
+}
---
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/entity/Role.java
@@ -0,0 +1,6 @@
+package com.wishal.SpringSecurityTokenLoginForm.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
