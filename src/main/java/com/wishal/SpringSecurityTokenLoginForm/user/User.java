5cd58406c8f2be1351ae2f9aa479a840c61d0c89
Wishal Siriwardana <wishal_s@epic.lk>
2023-08-09 10:33:27 +0530

    content updated

--- src/main/java/com/wishal/SpringSecurityTokenLoginForm/user/User.java
+++ src/main/java/com/wishal/SpringSecurityTokenLoginForm/user/User.java
@@ -2,6 +2,7 @@ package com.wishal.SpringSecurityTokenLoginForm.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
